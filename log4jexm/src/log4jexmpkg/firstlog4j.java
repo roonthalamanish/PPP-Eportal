d664596264940adc6798e35198b0efe31a9fc80e
training_d5.01.02 <training_d5.01.02@DDC5-D-4VN4T72.dir.svc.accenture.com>
2018-07-18 11:11:04 +0530

    1st commit of java file

---
+++ log4jexm/src/log4jexmpkg/firstlog4j.java
@@ -0,0 +1,40 @@
+package log4jexmpkg;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class firstlog4j 
+{
+	private static Logger logger=Logger.getLogger("LoggingExample");
+	public static void main(String[] args)
+	{
+		System.out.println("Starting reading of Log file :- ");
+		
+		BasicConfigurator.configure();
+		try
+		{
+			FileInputStream fstream =new FileInputStream("C:\\Users\\training_d5.01.02\\Desktop\\MRPage.html");
+			DataInputStream in =new DataInputStream(fstream);
+			BufferedReader br =new BufferedReader(new InputStreamReader(in)); 
+			String strLine;
+			while ((strLine = br.readLine()) != null)
+				{System.out.println (strLine);	}
+		 in.close();
+		}		
+		catch (FileNotFoundException fe)
+		{
+			logger.error("File Not Found",fe);
+			logger.warn("This is a warning message");
+			logger.trace("This message will not be logged since log level is set as DEBUG");
+		}
+		catch (IOException e)
+		{logger.error("IOEXception occured:", e);}
+	}
+}
